2f86ba480ac9f6f16c8ff1abd685a688d7a8d3b1
AbhinavJawle <jawleabhinav7@gmail.com>
2024-12-24 00:18:24 +0530

    Initial working directory

--- src/cvbuilder.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import "./cvbuilder.css";
-
-function GeneralInfo({ name, email, phoneNumber }) {}
-
-function EducationalInfo({ schoolName, studyTitle, dateOfStudy }) {}
-
-function PracticalInfo({ companyName, positionTitle, role, timePeriod }) {}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import CVRender from "./cvrender.jsx";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CVRender />
+    <App />
   </StrictMode>
 );
